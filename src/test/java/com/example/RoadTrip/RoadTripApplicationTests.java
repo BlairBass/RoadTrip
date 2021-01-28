0f325c97b4448dec86fee3c5bf92ac9bbe5bd0d2
Greta <greta.sund@gmail.com>
2021-01-28 20:26:36 +0100

    First commit

---
+++ src/test/java/com/example/RoadTrip/RoadTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RoadTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
